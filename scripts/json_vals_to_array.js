46275f4e133b85cf5919038af2e090ba8f8420df
Michael Atavine <michael.atav@gmail.com>
2023-11-08 14:50:10 -0800

    Add parameters to sort JSON objects prior to returning their key values

--- scripts/json_vals_to_array.js
+++ scripts/json_vals_to_array.js
@@ -1,13 +1,28 @@
+const _ASCENDING_ORDER = "asc"
+const _DESCENDING_ORDER = "desc"
+
 /**
  * Extracts the value of every top-level key of every object in the specified JSON array.
  *
- * @param {string} jsonArray A JSON string containing an array of objects (e.g. [{"a": 1}, {"a": 2}])
- * @return An array of values from every top-level key of every object in the specified JSON array.
+ * @param {string} jsonArray A JSON string containing an array of objects.
+ * @param {string} sortKey Name of a top-level key to sort the JSON array objects by prior to returning their values (optional).
+ * @param {string} order The order by which to sort the array of JSON objects: "asc" for ascending order (default), "desc" for descending order. Requires sortKey to be applied (optional).
+ * @returns {Array} An array of values from every top-level key of every object in the specified JSON array.
  * @customfunction
  */
-function JSON_VALS_TO_ARRAY(jsonArray) {
-  const data = JSON.parse(jsonArray);
+function JSON_VALS_TO_ARRAY(jsonArray, sortKey = "", order = _ASCENDING_ORDER) {
+  let data = JSON.parse(jsonArray);
   const outputVals = new Array();
+
+  if (sortKey) {
+    order = order.toLowerCase();
+    if (order !== _ASCENDING_ORDER && order !== _DESCENDING_ORDER) {
+      return [`Invalid order specified, must be either: ${_ASCENDING_ORDER} or ${_DESCENDING_ORDER}`];
+    }
+
+    const orderMultiplier = (order == _ASCENDING_ORDER) ? 1 : -1;
+    data = data.sort((a, b) => ((a[sortKey] || 0) - (b[sortKey] || 0)) * orderMultiplier);
+  }
 
   for (const elem of data) {
     const keys = Object.keys(elem);
@@ -19,4 +34,3 @@ function JSON_VALS_TO_ARRAY(jsonArray) {
 
   return outputVals;
 }
-
